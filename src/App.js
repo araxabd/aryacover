4f6d5eb0f3bb0bb7cfbdf51842a50f3f51edeaa1
arianabdollahian <arianabdollahian@gmail.com>
2021-07-25 10:58:41 +0430

    add header and somthing else

--- src/App.js
+++ src/App.js
@@ -5,27 +5,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
-  // make states
-  const [mbicon, setmbicon] = useState(<FontAwesomeIcon icon={faBars} />);
-  const [mbturn, setmbturn] = useState(0);
-  // handle button click for menu
-  const menuClickHandler = (e) => {
-    if (mbturn === 0)
-    {
-      setmbicon(<FontAwesomeIcon icon={faTimes} />); // change icon 
-      setmbturn(1); // change turn to next step
-    } else {
-      setmbicon(<FontAwesomeIcon icon={faBars} />); // change icon to default
-      setmbturn(0); // set turn to default
-    }
-  };
-
   return (
     <>
-      <img src={logo} alt="آریا کاور تولید کننده کاور انواع پرینتر" id="logo" /> {/*logo*/}
-      <button onClick={menuClickHandler} id="menu-icon"> {/*menu button*/}
-        {mbicon}
-      </button>
+      <div id="header"><img src={logo} alt="آریا کاور تولید کننده کاور انواع پرینتر" id="logo" /> {/*logo*/}</div>
     </>
   );
 };
