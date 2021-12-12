f6a9a17a193f396fd3791d1ffde6bed8c362c0a8
arianabdollahian <arianabdollahian@gmail.com>
2021-12-12 11:55:29 +0330

    end react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { useState, useEffect } from "react";
-import "./App.css";
+import "./css/app.css";
 import logo from "./img/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import About from './components/About';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
     <>
-      <div id="header"><img src={logo} alt="آریا کاور تولید کننده کاور انواع پرینتر" id="logo" /> {/*logo*/}</div>
+      <div id="header">
+        <img src={logo} alt="آریا کاور تولید کننده کاور انواع پرینتر" id="logo" /> {/*logo*/}
+      </div>
+      <About></About>
     </>
   );
 };
