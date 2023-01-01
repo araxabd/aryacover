4403dd41ae8d3b20f9be5ec01d4bc326bfdd1581
araxabd <arianabdollahian@gmail.com>
2023-01-01 15:02:09 +0330

    some changes...

---
+++ assets/js/master.js
@@ -0,0 +1,10 @@
+
+const pfwrap = document.getElementById("pfwrap");
+
+const openModal = (e) => {
+    pfwrap.style.display = 'block';
+}
+
+const closeModal = (e) => {
+    pfwrap.style.display = 'none';
+}
